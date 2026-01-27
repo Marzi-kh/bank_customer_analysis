a2e66e46c927b9a02f1e68e0c99836a5f6d3e966
Marzi_kh <marzieh.khansarii@gmail.com>
2026-01-27 12:45:42 +0200

    Step 1-3 : data exploration, visualization, and cleaning

--- src/banking_analytics/load_file.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def load_data():
-    """load cleaned dataset"""
-    file_path = "/Users/skhansar19/PycharmProjects/bank_customer_analysis/data/bank.csv"
-    df = pd.read_csv(file_path)
-    return df
-
-df = load_data()
